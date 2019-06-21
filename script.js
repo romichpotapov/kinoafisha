e1a9c49f472a9f35f7d34acb3bf3a04c6a52339c
mosterbox <romapotapov7@gmail.com>
2019-06-21 09:04:07 +0300

    finished hw

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 const searchForm = document.querySelector('#search-form');
+const movie = document.querySelector('#movies');
 
 apiSearch = () => {
     event.preventDefault();
     const searchText = document.querySelector('.form-control').value,
     server = 'https://api.themoviedb.org/3/search/multi?api_key=ceec93d778f59df4556ed45a60299413&language=en-US&query=' + searchText;
     
-    requestApi('GET', server);
+    requestApi(server);
     
 }
 
 searchForm.addEventListener('submit', apiSearch);
 
-requestApi = ( method, url ) => {
+requestApi = ( url) => {
     const request = new XMLHttpRequest();
-    request.open(method, url);
+    request.open('GET', url);
+    request.send();
+
+    request.addEventListener('readystatechange', function() {
+        if (request.readyState !== 4) return;
+
+        if (request.status !== 200) {
+            console.log('error: ', request.status);
+            return;
+        }
+
+        const output = JSON.parse(request.responseText);
+        let inner = '';
+
+        console.log(output.results);
+        
+
+        output.results.forEach(function (item) {
+            let nameItem = item.name || item.title;
+            let releaseDate = item.release_date || 'Soon';
+            inner += `<div class="col-12 col-md-4 ">${nameItem}<span class="badge badge-secondary">${releaseDate}</span></div>`;
+            
+        });
+
+        movie.innerHTML = inner;
+    })
 }
